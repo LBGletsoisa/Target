8fa1590f0a8c018684a7987bd13680ad6abd960f
LBGletsoisa <105360000+LBGletsoisa@users.noreply.github.com>
2022-05-27 16:16:35 +0200

    Update index.py

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import urllib
 import re
 from bs4 import BeautifulSoup
 
-datafromwebsite=urllib.urlopen("https://api.thingspeak.com/channels/289288/feeds.json?results=2");
+datafromwebsite=urllib.urlopen("https://api.thingspeak.com/channels/289288/feeds.json?results=1");
 select=repr(datafromwebsite.read());
 select=select[300:];
 
